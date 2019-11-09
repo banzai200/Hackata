e149966e67f2b463565a4d30ca85d96aa1626f06
Wesley <guedes48@gmail.com>
2019-11-09 15:31:08 -0300

    very cool

--- main.php
+++ main.php
@@ -1,9 +1,9 @@
 <?php
-namespace main;
-require 'collector/correios.php';
-require 'collector/loggi.php';
-require 'collector/sql.php';
-require 'collector/jadlog.php';
+namespace collector;
+require 'correios.php';
+require 'loggi.php';
+require 'sql.php';
+require 'jadlog.php';
 
 
 public function exec($cep)
