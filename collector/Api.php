336dd94ff37c6f97a1626773c5b6dec8bedb3d78
WESLEY GUEDES PUTUMUJU <8939810@fmu.interno>
2019-11-09 13:42:28 -0200

    Criado Frete dos correios

--- collector/Api.php
+++ collector/Api.php
@@ -1,14 +1,36 @@
 <?php
 require 'vendor/autoload.php';
 use GuzzleHttp\Client;
+use EscapeWork\Frete\Correios\PrecoPrazo;
+use EscapeWork\Frete\Correios\Data;
+use EscapeWork\Frete\FreteException;
 
-$client = new Client();
+$cep = 49999;
 
-$test = $client->get('https://viacep.com.br/ws/01138000/json/');
+for($i = 11111;$i <= $cep; $i++){
+$frete = new PrecoPrazo();
+//echo $i;
+$frete->setCodigoServico(Data::SEDEX)
+      ->setCepOrigem('02363130')   # apenas numeros, sem hifen(-)
+      ->setCepDestino('064'.strval($i)) # apenas numeros, sem hifen(-)
+      ->setComprimento(30)              # obrigatorio
+      ->setAltura(30)                   # obrigatorio
+      ->setLargura(30)                  # obrigatorio
+      ->setDiametro(30)                 # obrigatorio
+      ->setPeso(50);                   # obrigatorio
+try {
+    $result = $frete->calculate();
+    echo 'cep : ' . '064'.strval($i);
+    echo 'valor :' . $result['cServico']['Valor'];
+    echo 'prazo :' . $result['cServico']['PrazoEntrega'];
 
-$test = $test->getBody()->getContents();
-
-$array = json_decode($test)->toArray();
-
-var_dump($array);
+   // var_dump($result); // debugge o resultado!
+}
+catch (FreteException $e) {
+    // trate o erro adequadamente (e não escrevendo na tela)
+    //echo $e->getMessage();
+    //echo $e->getCode(); // este código é o código de erro dos correios
+                        // pode ser usado pra dar mensagens como CEP inválido para o cliente
+}
+}
 ?>
