b2ed06a448b53b7e6ba4bef7428723b77ab872dd
Wesley <guedes48@gmail.com>
2019-11-10 10:33:20 -0300

    Montado o main

--- main.php
+++ main.php
@@ -1,14 +1,44 @@
 <?php
-namespace collector;
+require 'vendor/autoload.php';
 use EscapeWork\Frete\Correios\PrecoPrazo;
-
-require 'collector/correios.php';
-require 'collector/sql.php';
+use EscapeWork\Frete\Correios\Data;
+use EscapeWork\Frete\FreteException;
+use EscapeWork\Frete\Correios\ConsultaCEP;
+use GuzzleHttp\client;
 require 'collector/jadlog.php';
+$fuck = new main;
+$fuck->correios('01138000', '02363130', '45', '45', '15', '15', '15');
 
-function exec($cep)
+$jadlog = new \Jadlog();
+
+class main{
+
+//$fuck = new GuzzleHttp\Client;
+//$no = $fuck->post('https://www.motoboy.com/apiV1/orcamento?cidade=sp%2Fsao-paulo&endereco1_cep=05016000&endereco2_cep=01138000');
+//echo $no->getBody()->getContents();
+public function correios($cepOrigem, $cepDestino, $comprimento, $altura, $largura, $diametro, $peso)
 {
     $correios = new PrecoPrazo();
-    $jadlog = new \Jadlog();
-}
+    $correiosEnd = new ConsultaCEP();
+    $correios->setCodigoServico(Data::$codigos)
+        ->setCepOrigem($cepOrigem)
+        ->setCepDestino($cepDestino)
+        ->setComprimento($comprimento)
+        ->setAltura($altura)
+        ->setLargura($largura)
+        ->setDiametro($diametro)
+        ->setPeso($peso);
 
+    try {
+        $result = $correios->calculate();
+        print_r($result);
+    }
+    catch (FreteException $e) {
+        print_r($e->getMessage());
+        return $e->getMessage();
+
+    }
+}
+function motoboy($cepOrigem, $cepDestino, $cidade){
+
+}}
